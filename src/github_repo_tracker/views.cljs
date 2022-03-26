8026a1e29272c64a352691a2abeb19488c10699a
hughdani <daniel_hugh@hotmail.com>
2022-03-26 14:25:30 -0400

    Rename re-frame alias to rf

--- src/github_repo_tracker/views.cljs
+++ src/github_repo_tracker/views.cljs
@@ -1,11 +1,11 @@
 (ns github-repo-tracker.views
   (:require
-   [re-frame.core :as re-frame]
+   [re-frame.core :as rf]
    [github-repo-tracker.subs :as subs]
    ))
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
+  (let [name (rf/subscribe [::subs/name])]
     [:div
      [:h1
       "Hello from " @name]
