2967460daea41b16fc1c7812c98fc704d720c9c7
hughdani <daniel_hugh@hotmail.com>
2022-03-27 16:27:22 -0400

    Retrieve/persist repo changes to/from LocalStorage

--- src/github_repo_tracker/db.cljs
+++ src/github_repo_tracker/db.cljs
@@ -1,4 +1,6 @@
-(ns github-repo-tracker.db)
+(ns github-repo-tracker.db
+  (:require [cljs.reader]
+            [re-frame.core :as rf]))
 
 (def app-db-schema
   [:map
@@ -13,3 +15,21 @@
 
 (def default-db
   {:repos {}})
+
+;; Local Storage  ----------------------------------------------------------
+
+(def ls-key "github-repo-tracker")
+
+(defn repos->local-store
+  [{:keys [repos]}]
+  (.setItem js/localStorage ls-key (str repos)))
+
+;; cofx Registrations  -----------------------------------------------------
+
+(rf/reg-cofx
+ ::local-store-repos
+ (fn [cofx _]
+   (assoc cofx :local-store-repos
+          (into (hash-map)
+                (some->> (.getItem js/localStorage ls-key)
+                         (cljs.reader/read-string))))))
