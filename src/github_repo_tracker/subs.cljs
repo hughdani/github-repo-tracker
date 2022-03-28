2a948ddfa98473fe1c13aa81b8c3a7acd8f8570b
hughdani <daniel_hugh@hotmail.com>
2022-03-27 22:49:02 -0400

    Add release date related events

--- src/github_repo_tracker/subs.cljs
+++ src/github_repo_tracker/subs.cljs
@@ -6,3 +6,31 @@
  ::repos
  (fn [db]
    (vals (:repos db))))
+
+(rf/reg-sub
+ ::repo-by-id
+ (fn [db [_ id]]
+   (get-in db [:repos id])))
+
+(rf/reg-sub
+ ::latest-release-date-str-by-id
+ (fn [[_ id] _]
+   (rf/subscribe [::repo-by-id id]))
+
+ (fn [repo [_ id]]
+   (when-let [published-at (get-in repo [:latest-release :published_at])]
+     (.toLocaleDateString (js/Date. published-at)))))
+
+(comment
+  ;; babashka
+  @(rf/subscribe [::repo-by-id 201467090])
+  @(rf/subscribe [::latest-release-date-str-by-id 201467090])
+
+  ;; calva
+  @(rf/subscribe [::repo-by-id 125431277])
+  @(rf/subscribe [::latest-release-date-str-by-id 125431277])
+
+  ;; shadow-cljs (no releases)
+  @(rf/subscribe [::repo-by-id 43973779])
+  @(rf/subscribe [::latest-release-date-str-by-id 43973779])
+  )
